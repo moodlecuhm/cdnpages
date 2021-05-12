4a8c754e3b6e69b6f784b1116241b90cca956ea6
moodlecuhm <83899352+moodlecuhm@users.noreply.github.com>
2021-05-12 15:08:38 -0500

    Add files via upload

--- service_moodlecuhm.js
+++ service_moodlecuhm.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(event)
 
     //https://recorrido.cuhm.mx/wp-json/wp/v2/posts?categories=13
 
-    axios.get('https://recorrido.cuhm.mx/wp-json/wp/v2/posts?categories=13', {
+    axios.get('https://recorrido.cuhm.mx/744-2/', {
         headers: {
             'Content-Type': 'application/json',
         }
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function(event)
 
     .then(function(res) 
     {  
-        console.log('correcto ok'); 
-        // if(res.status == 200) 
-        // {
+        //console.log('correcto ok'); 
+        if(res.status == 200) 
+        {
         // //   console.log('resultado post: ' + res.data[0].id);          
         // //   console.log('header: ' + res.headers['x-wp-total']);
-        //     console.log('correcto');
-        // }
+             console.log('correcto');
+        }
     })
     
     .catch(function(err) 
